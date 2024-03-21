d88763e090da2727c41fef03f12c9ac50152f3e1
olivergraininge <oliver@grainge.me>
2024-03-21 08:36:46 +0000

    Adding dataset changes

--- datasets.py
+++ datasets.py
@@ -11,7 +11,7 @@ import utils
 
 
 class KittiDataset:
-    def __init__(self, config: dict, sequence: str = "11"):
+    def __init__(self, config: dict, sequence: str = "09"):
         super().__init__()
         self.data_dir = config["datasets_directory"]
         self.sequence_dir = join(
