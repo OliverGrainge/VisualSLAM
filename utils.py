f4be111979a12917a8929066fa380811fd138508
olivergraininge <oliver@grainge.me>
2024-03-21 09:07:29 +0000

    Added docstrings to utils

--- utils.py
+++ utils.py
@@ -16,12 +16,30 @@ np.set_printoptions(precision=3, suppress=True)
 
 
 def get_config():
+    """
+    Loads and returns the configuration data from a YAML file named 'config.yaml'.
+
+    Returns:
+        dict: A dictionary containing the configuration data.
+    """
+
     with open("config.yaml", "r") as file:
         data = yaml.safe_load(file)
     return data
 
 
 def get_feature_detector():
+    """
+    Creates and returns a feature detector object based on the configuration specified in 'config.yaml'.
+
+    The configuration file should specify the type of feature detector to use under 'feature_detector'.
+
+    Returns:
+        point_features.ORB or point_features.SIFT: An instance of the specified feature detector.
+
+    Raises:
+        NotImplementedError: If the feature detector specified in the configuration is not supported.
+    """
     config = get_config()
     if "orb" in config["feature_detector"].lower():
         return point_features.ORB()
@@ -32,6 +50,19 @@ def get_feature_detector():
 
 
 def get_feature_matcher():
+    """
+    Creates and returns a feature matcher object based on the configuration specified in 'config.yaml'.
+
+    The configuration file should specify the type of feature detector to use under 'feature_detector', 
+    which determines the appropriate feature matcher to be returned.
+
+    Returns:
+        cv2.BFMatcher: An instance of a brute force matcher with the appropriate norm type.
+
+    Raises:
+        NotImplementedError: If the feature matcher for the specified feature detector is not supported.
+    """
+
     config = get_config()
     if "orb" in config["feature_detector"].lower():
         return cv2.BFMatcher(cv2.NORM_HAMMING)
@@ -43,7 +74,20 @@ def get_feature_matcher():
 
 def get_matches(
     matcher, desc1: np.ndarray, desc2: np.ndarray, ratio_threshold=0.75, top_N=None
-):
+) -> List[cv2.KeyPoint.KeyPoint]:
+    """
+    Finds and filters matches between two sets of descriptors based on the ratio test and optionally selects the top N matches.
+
+    Parameters:
+        matcher: The matcher object to use for finding matches.
+        desc1 (np.ndarray): The first set of descriptors.
+        desc2 (np.ndarray): The second set of descriptors.
+        ratio_threshold (float, optional): The threshold for the Lowe's ratio test. Defaults to 0.75.
+        top_N (int, optional): The number of top matches to select based on their distance. If None, all good matches are returned.
+
+    Returns:
+        list: A list of filtered matches.
+    """
     matches = matcher.knnMatch(desc1, desc2, k=2)
     good_matches = []
     for m, n in matches:
@@ -56,7 +100,19 @@ def get_matches(
     return matches
 
 
-def sort_matches(matches: List, left_kp: List, right_kp: List, left_desc: np.ndarray):
+def sort_matches(matches: List, left_kp: List, right_kp: List, left_desc: np.ndarray) -> Tuple[List[cv2.KeyPoint.KeyPoint], List[cv2.KeyPoint.KeyPoint], np.ndarray]:
+    """
+    Sorts matches and extracts corresponding points and descriptors.
+
+    Parameters:
+        matches (List): A list of matches.
+        left_kp (List): Key points from the left image.
+        right_kp (List): Key points from the right image.
+        left_desc (np.ndarray): Descriptors from the left image.
+
+    Returns:
+        tuple: Three numpy arrays containing the sorted points from the left and right images, and the sorted descriptors from the left image.
+    """
     points_left = np.zeros((len(matches), 2))
     points_right = np.zeros((len(matches), 2))
     points_desc = np.zeros((len(matches), left_desc.shape[1]), dtype=np.float32)
@@ -68,6 +124,16 @@ def sort_matches(matches: List, left_kp: List, right_kp: List, left_desc: np.nda
 
 
 def homogenize(rotation: np.ndarray, translation: np.ndarray) -> np.ndarray:
+    """
+    Converts rotation and translation vectors into a homogeneous transformation matrix.
+
+    Parameters:
+        rotation (np.ndarray): The rotation vector.
+        translation (np.ndarray): The translation vector.
+
+    Returns:
+        np.ndarray: A 4x4 homogeneous transformation matrix.
+    """
     transformation = np.eye(4)
     R, _ = cv2.Rodrigues(rotation)
     transformation[:3, :3] = R.squeeze()
@@ -76,6 +142,15 @@ def homogenize(rotation: np.ndarray, translation: np.ndarray) -> np.ndarray:
 
 
 def unhomogenize(pose: np.ndarray) -> Tuple[np.ndarray]:
+    """
+    Converts a 4x4 homogeneous transformation matrix into rotation and translation vectors.
+
+    Parameters:
+        pose (np.ndarray): A 4x4 homogeneous transformation matrix.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: Rotation and translation vectors extracted from the transformation matrix.
+    """
     assert pose.shape[0] == 4
     assert pose.shape[1] == 4
     rot = pose[:3, :3]
@@ -85,6 +160,16 @@ def unhomogenize(pose: np.ndarray) -> Tuple[np.ndarray]:
 
 
 def transform_points3d(trans: np.ndarray, points: np.ndarray):
+    """
+    Applies a 4x4 transformation matrix to a set of 3D points.
+
+    Parameters:
+        trans (np.ndarray): A 4x4 transformation matrix.
+        points (np.ndarray): A set of 3D points.
+
+    Returns:
+        np.ndarray: The transformed 3D points.
+    """
     points = np.hstack((points, np.ones((len(points), 1))))
     assert points.shape[1] == 4
     tpoints = np.dot(trans, points.T).T
@@ -93,6 +178,15 @@ def transform_points3d(trans: np.ndarray, points: np.ndarray):
 
 
 def decompose_projection(proj: np.ndarray) -> np.ndarray:
+    """
+    Decomposes a projection matrix into its intrinsic matrix, rotation matrix, and translation vector components.
+
+    Parameters:
+        proj (np.ndarray): A projection matrix.
+
+    Returns:
+        tuple: The intrinsic matrix, rotation matrix, and translation vector.
+    """
     K, R, T = cv2.decomposeProjectionMatrix(proj)[:3]
     T = T.flatten()
     T = T[:3] / T[3]
@@ -101,6 +195,17 @@ def decompose_projection(proj: np.ndarray) -> np.ndarray:
 
 
 def projection_matrix(rvec: np.ndarray, tvec: np.ndarray, k: np.ndarray):
+    """
+    Constructs a projection matrix from rotation and translation vectors and an intrinsic matrix.
+
+    Parameters:
+        rvec (np.ndarray): A rotation vector.
+        tvec (np.ndarray): A translation vector.
+        k (np.ndarray): An intrinsic matrix.
+
+    Returns:
+        np.ndarray: The constructed projection matrix.
+    """
     assert len(rvec.squeeze()) == 3
     assert len(tvec.squeeze()) == 3
     proj = np.eye(4)[:3, :]
@@ -120,7 +225,29 @@ def relative_transformation(
     left_kp: List,
     desc2d: np.ndarray,
     K_l: np.ndarray,
-):
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Estimates the relative camera pose (rotation and translation vectors) between a 3D scene and its 2D image projection using the Perspective-n-Point (PnP) algorithm.
+
+    This function matches 3D descriptors with 2D descriptors, sorts the correspondences to align 3D points with their 2D projections, and then applies the PnP algorithm to estimate the relative pose.
+
+    Parameters:
+        matcher: The matcher object used to find matches between descriptors.
+        points3d (np.ndarray): A Numpy array of 3D points in the scene.
+        desc3d (np.ndarray): A Numpy array of descriptors corresponding to the 3D points.
+        left_kp (List): A list of keypoints in the 2D image, typically obtained from a feature detector.
+        desc2d (np.ndarray): A Numpy array of descriptors corresponding to the keypoints in the 2D image.
+        K_l (np.ndarray): The intrinsic camera matrix of the 2D image's capturing camera.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: A tuple containing the rotation vector and translation vector that represent the estimated camera pose.
+
+    Raises:
+        Exception: If the PnP algorithm fails to find a valid solution.
+
+    Note:
+        The PnP algorithm is applied using RANSAC for robustness against outliers.
+    """
     matches = get_matches(matcher, desc3d, desc2d, top_N=None)
     points3d_sorted = np.zeros((len(matches), 3), dtype=np.float32)
     points2d_sorted = np.zeros((len(matches), 2), dtype=np.float32)
