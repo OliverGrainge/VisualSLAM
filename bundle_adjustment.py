c4741570b9f067ac88f3d49c4acfbed673de0be5
oliver grainge <oliver@grainge.me>
2024-04-15 16:02:20 +0100

    initial push to branch

---
+++ bundle_adjustment.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+
+
+
+
+class WindowedBundleAdjustment:
+    def __init__(self, window: int=5):
+        super().__init__()
+        self.poses = []
+        self.images = []
+
+
+    def track_features():
+        pass 
+
+    def triangulate_points():
+        pass 
+
+
+
+    def bundle_adjust():
+        pass 
